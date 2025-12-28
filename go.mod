0ea55abcd259892f14ba98f975efa47d6c807506
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-28 13:32:57 +0700

    Replace viper with carlos0/env for env parsingn

--- go.mod
+++ go.mod
@@ -6,31 +6,22 @@ replace github.com/bencoronard/demo-go-common-libs => ../demo-go-common-libs
 
 require (
 	github.com/bencoronard/demo-go-common-libs v0.0.0-20251226151522-4afb856ceb8a
-	github.com/go-viper/mapstructure/v2 v2.4.0
+	github.com/caarlos0/env/v11 v11.3.1
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/labstack/echo/v4 v4.14.0
-	github.com/spf13/viper v1.21.0
 	go.uber.org/fx v1.24.0
 	go.uber.org/zap v1.27.1
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
-	github.com/sagikazarmark/locafero v0.12.0 // indirect
-	github.com/spf13/afero v1.15.0 // indirect
-	github.com/spf13/cast v1.10.0 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/dig v1.19.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
--- internal/config/properties.go
+++ internal/config/properties.go
@@ -1,81 +1,49 @@
 package config
 
-import (
-	"github.com/go-viper/mapstructure/v2"
-	"github.com/spf13/viper"
-)
+import "github.com/caarlos0/env/v11"
+
+type Properties struct {
+	Env EnvCfg
+}
 
 func NewProperties() (*Properties, error) {
-	v := viper.New()
-	v.AutomaticEnv()
-
-	keys := []string{
-		"APP_LISTEN_PORT",
-		"APP_ENVIRONMENT",
-		"APP_PUBLIC_KEY_PATH",
-		"VAULT_URI", "VAULT_TOKEN",
-		"OTEL_COL_METRICS_ENDPOINT",
-		"OTEL_COL_TRACES_ENDPOINT",
-		"OTEL_COL_LOGS_ENDPOINT",
-		"OTEL_METRICS_SAMPLING_FREQ_IN_MIN",
-		"OTEL_TRACES_SAMPLING_PROBABILITY",
-		"CRUD_API_DB_CP_CAP",
-		"CRUD_API_DB_CP_IDLE_MIN",
-		"CRUD_API_DB_CP_IDLE_TIMEOUT",
-		"CRUD_API_DB_CP_CONN_TIMEOUT",
-		"CRUD_API_DB_CP_CONN_TTL",
-	}
-
-	for _, key := range keys {
-		v.BindEnv(key)
-	}
-
 	var props Properties
-	err := v.Unmarshal(&props, func(c *mapstructure.DecoderConfig) {
-		c.TagName = "mapstructure"
-	})
-
-	if err != nil {
+	if err := env.Parse(&props); err != nil {
 		return nil, err
 	}
-
 	return &props, nil
 }
 
-type Properties struct {
-	Env EnvCfg `mapstructure:",squash"`
-}
-
 type EnvCfg struct {
-	App   AppCfg   `mapstructure:",squash"`
-	Vault VaultCfg `mapstructure:",squash"`
-	OTEL  OTELCfg  `mapstructure:",squash"`
-	DB    DBCfg    `mapstructure:",squash"`
+	App   AppCfg
+	Vault VaultCfg
+	OTEL  OTELCfg
+	DB    DBCfg
 }
 
 type AppCfg struct {
-	ListenPort    int    `mapstructure:"APP_LISTEN_PORT"`
-	Environment   string `mapstructure:"APP_ENVIRONMENT"`
-	PublicKeyPath string `mapstructure:"APP_PUBLIC_KEY_PATH"`
+	ListenPort    int    `env:"APP_LISTEN_PORT"`
+	Environment   string `env:"APP_ENVIRONMENT"`
+	PublicKeyPath string `env:"APP_PUBLIC_KEY_PATH"`
 }
 
 type VaultCfg struct {
-	URI   string `mapstructure:"VAULT_URI"`
-	Token string `mapstructure:"VAULT_TOKEN"`
+	URI   string `env:"VAULT_URI"`
+	Token string `env:"VAULT_TOKEN"`
 }
 
 type OTELCfg struct {
-	MetricsEndpoint           string  `mapstructure:"OTEL_COL_METRICS_ENDPOINT"`
-	TracesEndpoint            string  `mapstructure:"OTEL_COL_TRACES_ENDPOINT"`
-	LogsEndpoint              string  `mapstructure:"OTEL_COL_LOGS_ENDPOINT"`
-	MetricsSamplingFreqInMin  string  `mapstructure:"OTEL_METRICS_SAMPLING_FREQ_IN_MIN"`
-	TracesSamplingProbability float64 `mapstructure:"OTEL_TRACES_SAMPLING_PROBABILITY"`
+	MetricsEndpoint           string  `env:"OTEL_COL_METRICS_ENDPOINT"`
+	TracesEndpoint            string  `env:"OTEL_COL_TRACES_ENDPOINT"`
+	LogsEndpoint              string  `env:"OTEL_COL_LOGS_ENDPOINT"`
+	MetricsSamplingFreqInMin  string  `env:"OTEL_METRICS_SAMPLING_FREQ_IN_MIN"`
+	TracesSamplingProbability float64 `env:"OTEL_TRACES_SAMPLING_PROBABILITY"`
 }
 
 type DBCfg struct {
-	ConnectionPoolCap         int `mapstructure:"CRUD_API_DB_CP_CAP"`
-	ConnectionPoolIdleMin     int `mapstructure:"CRUD_API_DB_CP_IDLE_MIN"`
-	ConnectionPoolIdleTimeout int `mapstructure:"CRUD_API_DB_CP_IDLE_TIMEOUT"`
-	ConnectionTimeout         int `mapstructure:"CRUD_API_DB_CP_CONN_TIMEOUT"`
-	ConnectionTTL             int `mapstructure:"CRUD_API_DB_CP_CONN_TTL"`
+	ConnectionPoolCap         int `env:"CRUD_API_DB_CP_CAP"`
+	ConnectionPoolIdleMin     int `env:"CRUD_API_DB_CP_IDLE_MIN"`
+	ConnectionPoolIdleTimeout int `env:"CRUD_API_DB_CP_IDLE_TIMEOUT"`
+	ConnectionTimeout         int `env:"CRUD_API_DB_CP_CONN_TIMEOUT"`
+	ConnectionTTL             int `env:"CRUD_API_DB_CP_CONN_TTL"`
 }
