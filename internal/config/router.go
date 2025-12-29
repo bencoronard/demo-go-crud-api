2d538de311061357933ff39444d10edd7f2b470f
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-29 16:26:00 +0700

    Rename HttpRouter to Router

--- internal/config/router.go
+++ internal/config/router.go
@@ -1,16 +1,55 @@
 package config
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/bencoronard/demo-go-crud-api/internal/resource"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
-func NewRouter(h *resource.ResourceHandler) *echo.Echo {
+type Router interface {
+	Start(lc fx.Lifecycle, props *Properties)
+}
+
+type routerImpl struct {
+	r   *echo.Echo
+	h   *resource.ResourceHandler
+	log *zap.Logger
+}
+
+func NewRouter(h *resource.ResourceHandler, l *zap.Logger) Router {
 	r := echo.New()
 	registerMiddlewares(r)
 	registerRoutes(r, h)
-	return r
+	return &routerImpl{
+		r:   r,
+		h:   h,
+		log: l,
+	}
+}
+
+func (r *routerImpl) Start(lc fx.Lifecycle, props *Properties) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			r.log.Info(fmt.Sprintf("Process ID: %d on %s", os.Getpid(), props.Env.App.Environment))
+			go func() {
+				if err := r.r.Start(fmt.Sprintf(":%d", props.Env.App.ListenPort)); err != nil && err != http.ErrServerClosed {
+					r.log.Error("HTTP server failed to start", zap.Error(err))
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			r.log.Info("Shutting down HTTP server...")
+			return r.r.Shutdown(ctx)
+		},
+	})
 }
 
 func registerMiddlewares(r *echo.Echo) {
--- internal/server/container.go
+++ internal/server/container.go
@@ -1,16 +1,9 @@
 package server
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-
 	"github.com/bencoronard/demo-go-crud-api/internal/config"
 	"github.com/bencoronard/demo-go-crud-api/internal/resource"
-	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 func Start() {
@@ -27,25 +20,7 @@ func Start() {
 			config.NewRouter,
 		),
 		fx.Invoke(
-			startServer,
+			config.Router.Start,
 		),
 	).Run()
-}
-
-func startServer(lc fx.Lifecycle, r *echo.Echo, log *zap.Logger, props *config.Properties) {
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			log.Info(fmt.Sprintf("Process ID: %d on %s", os.Getpid(), props.Env.App.Environment))
-			go func() {
-				if err := r.Start(fmt.Sprintf(":%d", props.Env.App.ListenPort)); err != nil && err != http.ErrServerClosed {
-					log.Error("HTTP server failed to start", zap.Error(err))
-				}
-			}()
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			log.Info("Shutting down HTTP server...")
-			return r.Shutdown(ctx)
-		},
-	})
 }
