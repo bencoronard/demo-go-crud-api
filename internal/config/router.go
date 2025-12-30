d33f2c7c118b505175dba69bb2592410d831395e
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-30 21:08:37 +0700

    Add listening port logging

--- internal/config/router.go
+++ internal/config/router.go
@@ -3,7 +3,9 @@ package config
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/bencoronard/demo-go-crud-api/internal/resource"
 	"github.com/labstack/echo/v4"
@@ -23,6 +25,7 @@ func NewRouter(lc fx.Lifecycle, h *resource.ResourceHandler, p *Properties) *htt
 
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
+			slog.Info(fmt.Sprintf("[PID: %d] Server listening on port: %d", os.Getpid(), p.Env.App.ListenPort))
 			return nil
 		},
 		OnStop: func(ctx context.Context) error {
